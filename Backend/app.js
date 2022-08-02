3e0fdf6c52c42aec0ea2ff142c7193e4279082cd
anjoos2006 <anjoos20@gmail.com>
2022-08-02 10:52:57 +0530

    heroku port correction

--- Backend/app.js
+++ Backend/app.js
@@ -8,6 +8,8 @@ const path = require('path');
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 // Middleware
 app.use(express.urlencoded({extended: true})); 
 app.use(express.json());
